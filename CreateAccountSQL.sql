c089a0f0b0cb0333771fd7ccc26b0b09bc7827aa
Mai Bảo Long <66893806+zitsieunhan1512@users.noreply.github.com>
2023-05-04 22:20:24 +0700

    Cập nhật query

--- CreateAccountSQL.sql
+++
@@ -1,2 +0,0 @@
-﻿INSERT INTO TaiKhoans (TenTK, MatKhau, HoVaTen, NgaySinh, CCCD, DiaChi, SDT, IsAdmin, SoDu, DiemTinDung, NgayMoTaiKhoan, DanhSachDen)
-VALUES ('user1', 'pass1', 'Nguyen Van A', '1990-01-01', '123456789012', 'Hanoi', '0123456789', 0, 1000000, 100, '2023-05-04', 0);
---
+++ Query tạo Account.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO TaiKhoans (TenTK, MatKhau, HoVaTen, NgaySinh, CCCD, DiaChi, SDT, IsAdmin, SoDu, DiemTinDung, NgayMoTaiKhoan, DanhSachDen)
+VALUES ('Admin', '123456', 'Nguyen Van A', '1990-01-01', '123456789012', 'Hanoi', '0123456789', 1, 100000000, 100, '2023-05-04', 0);
+
+INSERT INTO TaiKhoans (TenTK, MatKhau, HoVaTen, NgaySinh, CCCD, DiaChi, SDT, IsAdmin, SoDu, DiemTinDung, NgayMoTaiKhoan, DanhSachDen)
+VALUES ('user1', '123456', 'Nguyen Van A', '1990-01-01', '123456789012', 'Hanoi', '0123456789', 0, 100000, 100, '2023-05-04', 0);
